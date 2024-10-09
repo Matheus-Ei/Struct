64ce3f3c02a491cb2a1db6dceb5c492573d83533
Matheus-Ei <t4igacomercial@gmail.com>
2024-10-09 17:33:07 -0300

    feat: back: add the auth system based on JWT and cookies HTTP-only

---
+++ back/src/services/cookie.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from "express";
+
+class Cookie {
+    public static generate(
+        name: string,
+        value: string | object,
+        res: Response,
+        options: {
+            maxAge?: number;
+            httpOnly?: boolean;
+            secure?: boolean;
+            sameSite?: "strict" | "lax" | "none";
+        } = {}
+    ) {
+        const cookieValue =
+            typeof value === "object" ? JSON.stringify(value) : value;
+
+        const cookieOptions = {
+            httpOnly: true,
+            secure: true,
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            sameSite: "strict" as "strict",
+            ...options,
+        };
+
+        res.cookie(name, cookieValue, cookieOptions);
+    }
+
+    public static delete(name: string, res: Response) {
+        res.clearCookie(name);
+    }
+
+    public static get(name: string, req: Request) {
+        return req.cookies[name]
+    }
+}
+
+export default Cookie;
--- back/src/system/routes.ts
+++ back/src/system/routes.ts
@@ -1,9 +1,11 @@
 import UserRoute from "../routes/user.js";
+import TokenRoute from "../routes/token.js";
 
 type MainRoutesType = Array<[String, Object]>
 
 const mainRoutes: MainRoutesType = [
     ["/users", UserRoute.router],
+    ["/token", TokenRoute.router],
 ];
 
 export default mainRoutes;
