ecb2cd436b0c6b7e6fba18dc32df7124ba5efb6c
Matheus-Ei <t4igacomercial@gmail.com>
2024-11-12 17:25:00 -0300

    fix: front: style error on new project modal

--- front/src/components/Input.tsx
+++ front/src/components/Input.tsx
@@ -37,7 +37,7 @@ const Input = ({
                 className={
                     className
                         ? className
-                        : "border rounded-btn h-14 w-[95%] pl-4 border-neutral mb-3 bg-base-100 text-base-content pt-4 resize-none"
+                        : "border rounded-btn outline-none h-14 w-[95%] pl-4 border-neutral mb-3 bg-base-100 text-base-content pt-4 resize-none"
                 }
                 placeholder={text}
                 onChange={handleChange}
